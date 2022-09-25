4feb7efff98c4fbaf9d59462010936b50f2fe0c2
William Liono Lie <william.lie@mail.utoronto.ca>
2022-09-25 12:42:33 -0400

    Implement Generate Full valid sudoku

---
+++ generate_sudoku.py
@@ -0,0 +1,55 @@
+import copy
+import random
+
+
+def possible_value(i:int, sudoku: list[list[int]]):
+    poss = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    x = i // 9
+    y = i % 9
+    for i in range(0, 9):
+        if sudoku[x][i] in poss:
+            poss.remove(sudoku[x][i])
+        if sudoku[i][y] in poss:
+            poss.remove(sudoku[i][y])
+
+    square = (x//3, y//3)
+    for i in range(0, 3):
+        x_check = square[0] * 3 + i
+        for j in range(0, 3):
+            y_check = square[1] * 3 + j
+            if sudoku[x_check][y_check] in poss:
+                poss.remove(sudoku[x_check][y_check])
+    return poss
+
+
+def generate_full_recur(i: int, sudoku: list[list[int]]):
+
+    if i == 81:
+        return True, sudoku
+
+    x = i // 9
+    y = i % 9
+    poss = possible_value(i, sudoku)
+    print(i, sudoku, poss)
+
+    if len(poss) == 0:
+        return False, sudoku
+
+    for j in range(len(poss)):
+        curr = random.choice(tuple(poss))
+        poss.remove(curr)
+        sudoku_curr = copy.deepcopy(sudoku)
+        sudoku_curr[x][y] = curr
+        res, out = generate_full_recur(i + 1, sudoku_curr)
+        if res:
+            return True, out
+
+    return False, sudoku
+
+def generate_full():
+    sudoku = [[0] * 9 for x in range(0, 9)]
+    return generate_full_recur(0, sudoku)
+
+def generate_sudoku():
+    map = []
+
